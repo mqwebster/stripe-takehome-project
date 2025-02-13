0126f8df0d0d6b482b1a67c0822f39927c0b623c
Marques Webster <marques.q.webster@gmail.com>
2025-02-12 21:19:58 -0600

    added Navbar and basic styling

---
+++ src/components/wrappers/Navbar/Navbar.tsx
@@ -0,0 +1,37 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Navbar() {
+  return (
+    <header className="sticky top-0 border-b-2 border-orange-base">
+      <div className="container py-md w-full flex justify-between items-center">
+        <div className="w-2/5">
+          <Image
+            src={"/logo.png"}
+            width={4096}
+            height={600}
+            alt="Cozy Threads Logo"
+            className="w-full"
+          />
+        </div>
+
+        <div>
+          <nav className="flex gap-4">
+            <Link
+              href={"https://mqwebster.com"}
+              target="_blank"
+              className=" text-sm"
+            >
+              Visit my portfolio
+            </Link>
+          </nav>
+
+          <div>
+            {/* Saving this space for a checkout menu/button */}
+            {/* Should be built in separate file and imported here */}
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,29 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        orange: {
+          base: "#CA6F31",
+        },
+        yellow: {
+          base: "#F6CB40",
+        },
+        cream: {
+          base: "#F7E6DA",
+        },
+        black: {
+          base: "#1F1F1F",
+        },
+      },
+      spacing: {
+        xs: "4px",
+        sm: "8px",
+        md: "12px",
+        lg: "18px",
+        xl: "22px",
+        "2xl": "32px",
+        "3xl": "48px",
+        "4xl": "72px",
+        "5xl": "128px",
       },
     },
   },
