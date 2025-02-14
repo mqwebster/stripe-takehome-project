9bb55975e05d5d4d95e0531236bed3fe57fc86e3
Marques Webster <marques.q.webster@gmail.com>
2025-02-13 19:30:42 -0600

    added cart badge and icon to navbar

--- src/components/wrappers/Navbar/Navbar.tsx
+++ src/components/wrappers/Navbar/Navbar.tsx
@@ -1,9 +1,16 @@
+"use client";
+
+import { CartContext } from "@/components/Cart/CartContext";
 import Image from "next/image";
 import Link from "next/link";
+import { useContext } from "react";
+import { FiShoppingBag } from "react-icons/fi";
 
 export default function Navbar() {
+  const { cartItems } = useContext(CartContext);
+
   return (
-    <header className="sticky top-0 border-b-2 border-orange-base bg-cream-base">
+    <header className="sticky top-0 z-40 border-b-2 border-orange-base bg-cream-base">
       <div className="container py-md w-full flex justify-between items-center">
         <div className="w-2/5">
           <Link href={"/"}>
@@ -19,8 +26,16 @@ export default function Navbar() {
 
         <div>
           <nav className="flex gap-4">
-            <Link href={"/cart"} className=" text-sm">
-              Cart
+            <Link href={"/cart"} className="w-[28px] h-[28px] text-sm">
+              <div className="relative w-full h-full">
+                <FiShoppingBag size={28} color="#1f1f1f" />
+
+                {cartItems.length > 0 && (
+                  <div className="w-[20px] h-[20px] p-[2px] absolute -top-md -right-md flex items-center justify-center bg-orange-base text-cream-base text-xs rounded-full">
+                    {cartItems.length}
+                  </div>
+                )}
+              </div>
             </Link>
           </nav>
 
